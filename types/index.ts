8c2b9e01655f2deb4e1e1fdfccdf6b03d1511dad
Khouchane Mahmoud <97807779+kh-mahmoud@users.noreply.github.com>
2025-11-09 01:28:28 +0100

    upadate types

---
+++ types/index.ts
@@ -0,0 +1,34 @@
+import { countries } from '@/constants';
+import { Id } from '@/convex/_generated/dataModel';
+
+
+export type Country = typeof countries[number]["code"];
+
+export type scrapeProps = {
+  prompt: string;
+  country: Country;
+  reportId?:Id<'reports'>;
+};
+
+export type CountrySelectorProps = {
+  value: string;
+  onValueChange: (value: Country) => void;
+  disabled?: boolean;
+}
+
+export type RetryAnalysisResult = {
+  ok: boolean;
+  message: string;
+};
+
+export type ScrapingDataItem = {
+  prompt: string;
+  answer_text: string;
+  sources: Array<{
+    title: string;
+    url: string;
+    description: string;
+  }>;
+  timestamp: string;
+  url: string;
+}
